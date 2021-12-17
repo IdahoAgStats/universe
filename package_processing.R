84fc5d361105b803fac435ce6e74473898e6d396
Julia Piaskowski <julia.piask@gmail.com>
2021-12-17 12:43:01 -0800

    ctv file added

---
+++ package_processing.R
@@ -0,0 +1,18 @@
+
+library(stringr); library(jsonlite)
+
+packages <- read.csv("packages.txt")
+
+packages$github <- ifelse(str_detect(packages$url, "github.com/"),
+                               packages$url, 
+                          file.path("https://github.com/cran", packages$package))
+
+write.csv(packages, "packages.csv")
+
+packages2 <- packages[,-2]
+colnames(packages2)[2] <- "url"
+
+#package_json <- toJSON(packages)
+                          
+write_json(packages2, "packages.json", pretty = TRUE)
+
