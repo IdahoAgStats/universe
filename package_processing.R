26078e8a8342d1dd51dc3ba58bf77714ed67d659
jpiaskowski <julia.piask@gmail.com>
2022-05-11 15:50:01 -0700

    white space issue fixed hopefully

--- package_processing.R
+++ package_processing.R
@@ -2,9 +2,9 @@
 library(jsonlite); library(dplyr)
 
 packages <- read.csv("packages.csv") %>% filter(include == "yes") %>% 
-  select(package, github) %>% rename(url = "github")
+  select(package, github) %>% rename(url = "github") %>% 
+  mutate_all(., trimws, which = "both")
 
 #package_json <- toJSON(packages)
-                          
 write_json(packages, "packages.json", pretty = TRUE)
 
