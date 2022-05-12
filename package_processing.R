b86fd6fb4fa7f24285ef9cc269b60609454b46ed
Julia Piaskowski <julia.piask@gmail.com>
2022-05-12 12:03:58 -0700

    add subdir to json file

--- package_processing.R
+++ package_processing.R
@@ -1,9 +1,10 @@
 
 library(jsonlite); library(dplyr)
 
+
 packages <- read.csv("packages.csv") %>% filter(include == "TRUE") %>% 
-  select(package, github) %>% rename(url = "github") %>% 
-  mutate_all(., trimws, which = "both")
+  select(package, github, subdir) %>% rename(url = "github") %>% 
+  mutate_all(., trimws, which = "both") %>% distinct()
 
 #package_json <- toJSON(packages)
 write_json(packages, "packages.json", pretty = TRUE)
