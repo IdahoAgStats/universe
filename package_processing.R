e05151eacb437e0bc7b0b9d1220558ae8e59e70f
Julia Piaskowski <julia.piask@gmail.com>
2022-06-27 13:01:15 -0700

    cran pkg removed, git check added

--- package_processing.R
+++ package_processing.R
@@ -4,7 +4,11 @@ library(jsonlite); library(dplyr)
 
 packages <- read.csv("packages.csv") %>% filter(include == "TRUE") %>% 
   select(package, github) %>% rename(url = "github") %>% 
-  mutate_all(., trimws, which = "both") %>% distinct()
+  mutate_all(., trimws, which = "both") %>% distinct() %>% 
+  filter(grepl("git", url)) 
+
+pkg_no_cran <- filter(packages, !grepl("github.com/cran", url))
+
 
 #package_json <- toJSON(packages)
 write_json(packages, "packages.json", pretty = TRUE)
